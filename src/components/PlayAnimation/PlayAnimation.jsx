cd8618f60aa6ff75c33fb9a377f5568932d06101
kryptomario159 <marioarturo1407@gmail.com>
2022-02-19 01:29:47 -0600

    preyacasi

--- src/components/PlayAnimation/PlayAnimation.jsx
+++ src/components/PlayAnimation/PlayAnimation.jsx
@@ -1,11 +1,11 @@
 import "./playAnimation.scss"
 import { useEffect, useRef } from "react";
-import { useHistory } from "react-router-dom";
+/* import { useHistory } from "react-router-dom"; */
 import { TADUM_SOUND_URL } from "../../requests";
 
 const PlayAnimation = () => {
 
-	let history = useHistory();
+	/* let history = useHistory(); */
 	const soundRef = useRef(null);
 	const handleTadum = () => {
 		soundRef.current.currentTime = 0;
@@ -15,15 +15,15 @@ const PlayAnimation = () => {
 	useEffect(() => {
 		handleTadum();
 		setTimeout(() => {
-			history.push('/browse')
+			window.location.href = "https://sanpatricio.kazooie.io/"
 		}, 4200)
-	}, [history])
+	}, [])
 
 	return (
 		<div className='PlayAnimation__wrp'>
 			<audio ref={soundRef} src={TADUM_SOUND_URL} />
 			<span className="PlayAnimation__text">
-				FAKEFLIX
+				Metoozie
 			</span>
 		</div>
 	)
